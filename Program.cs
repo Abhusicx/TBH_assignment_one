06127a4d37f951216b7098041684cfab750adf18
Abhijeet thakur <Abhijeethakur94796@gmail.com>
2025-01-14 00:34:25 +0530

    TBP Assignment 1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using Assignment1;
+namespace Assignment1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            classStudent Parth = new classStudent();
+            Parth.Name = "parth Shrivastava";
+            Parth.Course = "TBP";
+            Parth.Age = 21;
+            Parth.Rollno = 134;
+            Parth.Display();
+
+            classStudent Abhijeet = new classStudent();
+            Abhijeet.Name = "Abhijeet thakur";
+            Abhijeet.Course = "TBP";
+            Abhijeet.Age = 21;
+            Abhijeet.Rollno = 1;
+            Abhijeet.Display();
+
+            classStudent Chandragupt = new classStudent();
+            Chandragupt.Name = "Chandragupt";
+            Chandragupt.Course = "ILP";
+            Chandragupt.Age = 21;
+            Chandragupt.Rollno = 17;
+            Chandragupt.Display();
+
+        }
+    }
+}
+
---
+++ classStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class classStudent
+    {
+        public string Name { get; set; }
+        public int Rollno { get; set; }
+        public string Course { get; set; }
+        public int Age { get; set; }
+
+    
+        public void Display()
+        {
+            Console.WriteLine("The student of name : " + Name + " of his age : " + Age + " of rollnumber : " + Rollno + " is enrolled in branch : " + Course);
+        }
+    }
+}
